f5b05b65ab316311fe9eff5063f03cf4a880cff3
Luis Villa <luisenvilla@MacBook-Pro-de-Luis.local>
2017-02-16 12:27:02 -0600

    DB connect IS-50 #done

--- Inverspot_API/index.js
+++ Inverspot_API/index.js
@@ -2,14 +2,29 @@
 
 const express = require('express')
 const config = require('./config')
-const app = express()
+const mongoose = require('mongoose')
 
-app.set ('port', config.server.port)
+const app = express()
+mongoose.connect(config.db.dbUri)
+
+app.set ('port', process.env.PORT || config.server.port)
 
 app.get ('/', (req,res) => {
   res.send ('Hello Word!!')
 })
 
 app.listen (app.get ('port'), (err) => {
-  console.log ("Server running on port ${app.get('port')}")
+  console.log (`Server running on port ${app.get("port")}`)
 })
+
+// Event Database
+mongoose.connection.on ('connected',() => {
+  console.log(`--> Database coneccted`)
+})
+mongoose.connection.on ('error', () => {
+  console.log(`--> EROR in database connect`)
+})
+mongoose.connection.on ('disconnected', () => {
+  console.log(`--> Database disconnected`)
+})
+// /Event Database
