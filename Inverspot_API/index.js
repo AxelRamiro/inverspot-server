f71c26b7c3f9ebcab3e89c69e594151fd9d9179b
Axel Ramiro <axel.ramiro.92@gmail.com>
2017-02-18 02:00:40 -0600

    Added CORS request support and user update return new data

--- Inverspot_API/index.js
+++ Inverspot_API/index.js
@@ -30,6 +30,13 @@ const authenticate = jwtMiddleware({
  }
 }).unless({path: ['/api/auth']})
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  next()
+})
+
 app.get ('/', (req,res) => {
   res.send ('Hello Word!!')
 })
--- Inverspot_API/src/api/user/update.js
+++ Inverspot_API/src/api/user/update.js
@@ -1,7 +1,8 @@
 module.exports = (router, User) => {
   router.put('/user', (req, res) => {
-    User.findByIdAndUpdate(req.body._id,req.body, (err,user) => {
-      if (err) return res.status(500).send(err.message)
+    User.findByIdAndUpdate(req.body._id, req.body, {new: true},(err, user) => {
+      if (err)
+        return res.status(500).send(err.message)
       res.status(200).jsonp(user)
     })
   })
