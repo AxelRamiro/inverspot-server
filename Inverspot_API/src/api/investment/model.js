26df5f31cffa47237acc5bc35489769e9776f6c7
LuisEnVilla <villak.hardware@gmail.com>
2017-02-18 20:40:02 -0600

    edit model Investment IS-41

--- Inverspot_API/src/api/investment/model.js
+++ Inverspot_API/src/api/investment/model.js
@@ -2,6 +2,7 @@ module.exports = (mongoose) => {
 
   let investmentSchema = new mongoose.Schema({
     sharesNumber:   Number,
+    amount:         Number,
     status:         String,
     userId:         {type:mongoose.Schema.Types.ObjectId, ref: 'User'},
     propertyId:     {type:mongoose.Schema.Types.ObjectId, ref: 'Property'}
