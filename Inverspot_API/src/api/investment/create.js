dc0c89a291d42b67d875ee4a406198ce33592c9d
LuisEnVilla <villak.hardware@gmail.com>
2017-02-20 19:14:40 -0600

    fixed model delete id*

--- Inverspot_API/src/api/investment/create.js
+++ Inverspot_API/src/api/investment/create.js
@@ -3,7 +3,7 @@ module.exports = (router, Investment, Property) => {
     let investment = new Investment (req.body)
     investment.save ((err, resInvestment) => {
       if (err) return res.status(500).send(err.message)
-      Property.findByIdAndUpdate(req.body.propertyId, { $inc: { 'dataSheet.sharesSold': req.body.sharesNumber}}, (err, resProperty) => {
+      Property.findByIdAndUpdate(req.body.property, { $inc: { 'dataSheet.sharesSold': req.body.sharesNumber}}, (err, resProperty) => {
         if (err) return res.status(500).send(err.message)
         res.status(201).jsonp(resInvestment)
       })
