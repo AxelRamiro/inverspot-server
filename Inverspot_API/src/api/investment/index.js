1368aa7d2706efb1df4fb64c187cbff7dd212993
LuisEnVilla <villak.hardware@gmail.com>
2017-02-22 03:45:01 -0600

    Update property shares Sold

--- Inverspot_API/src/api/investment/index.js
+++ Inverspot_API/src/api/investment/index.js
@@ -1,6 +1,6 @@
 module.exports = (router, Investment, Property, User, sendMail) => {
   require('./create')(router, Investment, Property, User, sendMail)
   require('./find')(router, Investment)
-  require('./update')(router, Investment)
+  require('./update')(router, Investment, Property)
   require('./delete')(router, Investment, Property)
 }
--- Inverspot_API/src/api/investment/update.js
+++ Inverspot_API/src/api/investment/update.js
@@ -1,11 +1,22 @@
-module.exports = (router, Investment) => {
+module.exports = (router, Investment, Property) => {
   router.put('/investment', (req, res) => {
 
-    Investment.findByIdAndUpdate(req.body._id, req.body, {new: true},(err, investment) => {
+    Investment.findById(req.body._id, (err, oldInvestment) => {
+      if (err)return res.status(500).send(err.message)
+      if (!oldInvestment) return res.status(404).send('NOT_FOUND')
 
-      if (err)
-        return res.status(500).send(err.message)
-      res.status(200).jsonp(investment)
+      Investment.findByIdAndUpdate(req.body._id, req.body, {new: true},(err, investment) => {
+
+        if (err)return res.status(500).send(err.message)
+        let dif = investment.sharesNumber - oldInvestment.sharesNumber
+
+        Property.findByIdAndUpdate(investment.property, {$inc:{'dataSheet.sharesSold': dif }}, {new: true}, (err, resProperty) =>{
+          if (err)return res.status(500).send(err.message)
+          if (!oldInvestment) return res.status(404).send('NOT_FOUND')
+          
+          res.status(200).jsonp(investment)
+        })
+      })
     })
   })
 }
