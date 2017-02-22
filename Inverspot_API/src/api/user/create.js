3cc9a43e21a98d43645e52d5888a0a7d2ad67933
LuisEnVilla <villak.hardware@gmail.com>
2017-02-21 20:34:14 -0600

    added mail welcome admin

--- Inverspot_API/src/api/user/create.js
+++ Inverspot_API/src/api/user/create.js
@@ -3,7 +3,7 @@ module.exports = (router, User, sendMail) => {
     let user = new User (req.body)
     user.save ((err, resUser) => {
       if (err) return res.status(500).send(err.message)
-      sendMail({to: resUser.email, subject: `Valida tu correo ${resUser.name}`}, 'recovery', {name: resUser.name, verifyUrl:resUser.checker}, console.log)
+      sendMail({to: resUser.email, subject: `¡${resUser.name}, Te damos la bienvenida a Inverspot!`}, 'welcome-admin', {email: resUser.email, password: req.body.password}, console.log)
       res.status(201).jsonp(resUser)
     })
   })
