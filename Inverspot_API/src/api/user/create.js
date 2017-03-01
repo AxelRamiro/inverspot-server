8e034bfc30e07972d10bc0f3bb28c500d898afa7
LuisEnVilla <villak.hardware@gmail.com>
2017-02-28 19:52:56 -0600

    documentation User Update

--- Inverspot_API/src/api/user/create.js
+++ Inverspot_API/src/api/user/create.js
@@ -1,9 +1,9 @@
 /**
-* @api {post} /user Crar usuario
+* @api {post} /user Crear usuario
 * @apiVersion 0.1.0
 * @apiName Crear Usuario
 * @apiGroup Usuario
-* @apiDescription Craa diferentes usuarios (User, Admin, Asesor y Invesment), automaticamente envia el correo de creacion de usuario por admin, con el correo y contraseña para logearse.
+* @apiDescription Crear diferentes usuarios (User, Admin, Asesor y Invesment), automáticamente envia el correo de creación de usuario por admin, con el correo y contraseña para loguearse.
 *
 * @apiParamExample {json} Body
 *   {
@@ -13,28 +13,26 @@
 *     "password": "123456",
 *     "level":  "user",
 *     "status": "inactive",
-*     "checker": "550e8400-e29b-41d4-a716-446655440000",
 *     "asesor":  "58afdb3f20b63a4893d1216c",
 *     "contactFrom": "Facebook",
 *     "invesmentData": {...}
 *   }
-*  @apiParam (body) {String} [name] Nombre de usuario.
-*  @apiParam (body) {String} email Correo de usuario, es obligatorio.
-*  @apiParam (body) {Number} [telephone] Numero telefonico del usuario.
-*  @apiParam (body) {String} password Contraseña del usuario, es obligatorio.
-*  @apiParam (body) {String} level =user Nivel del usuario, posibles [user,admin,asesor,invesment].
-*  @apiParam (body) {String} status =inactive Estado del usuario, por defecto esta inactivo hasta hacer la validacion con su correo [inactive,active].
-*  @apiParam (body) {String} checker Token de formato UUID v4, para hacer la activacion y recuperacion de contraseña, se genera automaticamente en el modelo User.
-*  @apiParam (body) {String} asesor Id del usuario asesor.
-*  @apiParam (body) {String} contactFrom De donde los contacta [Facebook].
-*  @apiParam (body) {Object} invesmentData Datos de inversionista, datos personales, datos bancarios y datos de beneficiarios, ver modelo User.
+*  @apiParam (Body) {String} [name] Nombre de usuario.
+*  @apiParam (Body) {String} email Correo de usuario, es obligatorio.
+*  @apiParam (Body) {Number} [telephone] Número telefónico del usuario.
+*  @apiParam (Body) {String} password Contraseña del usuario, es obligatorio.
+*  @apiParam (Body) {String} level =user Nivel del usuario, posibles [user,admin,asesor,invesment].
+*  @apiParam (Body) {String} status =inactive Estado del usuario, por defecto esta inactivo hasta hacer la validación con su correo [inactive,active].
+*  @apiParam (Body) {String} asesor Id del usuario asesor.
+*  @apiParam (Body) {String} contactFrom De donde los contacta [Facebook].
+*  @apiParam (Body) {Object} invesmentData Datos de inversionista, datos personales, datos bancarios y datos de beneficiarios, ver modelo User.
 *
-* @apiSuccessExample {json} 201
+* @apiSuccessExample {json} 201 CREATED
 *   {
 *     "name": "Lorem",
 *     "email": "a@a.com",
 *     "telephone": 1234567890,
-*     "password": "123456",
+*     "password": "$2a$10$cMZ362Mh7tC3ZqIJXi8H9Ob1vOFcf9lpP3yFvtEFWJOnYbp.qeNRy",
 *     "level":  "user",
 *     "status": "inactive",
 *     "checker": "550e8400-e29b-41d4-a716-446655440000",
@@ -46,10 +44,12 @@
 *    "createdAt": "2017-02-24T12:41:04.282Z",
 *   }
 *
-*  @apiSuccess (201) {Object} user Objeto de usuario, tal como lo manda la base de datos.
-*  @apiSuccess (201) {String} user._id Id de usuario, generado por la base de datos.
-*  @apiSuccess (201) {String} user.updatedAt Estampa de tiempo de la ultima actualizacion.
-*  @apiSuccess (201) {String} user.createdAt Estampa de tiempo de cuando fue creado.
+*  @apiSuccess (201 CREATED) {Object} user Objeto de usuario, tal como lo manda la base de datos.
+*  @apiSuccess (201 CREATED) {String} user._id Id de usuario, generado por la base de datos.
+*  @apiSuccess (201 CREATED) {String} user.password Contraseña incriptada.
+*  @apiSuccess (201 CREATED) {String} user.checker Token de formato UUID v4, para hacer la activación y recuperación de contraseña, se genera automáticamente en el modelo User.
+*  @apiSuccess (201 CREATED) {String} user.updatedAt Estampa de tiempo de la última actualización.
+*  @apiSuccess (201 CREATED) {String} user.createdAt Estampa de tiempo de cuando fue creado.
 *
 * @apiError (500) {String} Error  Mensaje de error del servidor, a la hora de crear el usuario.
 */
@@ -60,9 +60,9 @@ module.exports = (router, User, sendMail) => {
     // Guarda el nuvo usuario en la base de datos, en este punto todos los valores por defecto son agregados.
     user.save ((err, resUser) => {
       if (err) return res.status(500).send(err.message)
-      // Envio de Email de bienvenida tipo admin, es decir como si un administrador da de alta esta cuenta.
+      // Envio de Email de bienvenida tipo admin, es decir como si un administrador da de alta la cuenta.
       sendMail({to: resUser.email, subject: `¡${resUser.name}, Te damos la bienvenida a Inverspot!`}, 'welcome-admin', {email: resUser.email, password: req.body.password}, console.log)
-      // Regresa el Objeto usaurio completo como esta guardao en la base de dartos.
+      // Regresa el Objeto usaurio completo como está guardao en la base de dartos.
       res.status(201).jsonp(resUser)
     })
   })
