b49d1e7afd193395ff992e862c5a149fcf78ee5c
LuisEnVilla <villak.hardware@gmail.com>
2017-03-01 10:57:20 -0600

    define permission

---
+++ Inverspot_API/_apidoc.js
@@ -0,0 +1,5 @@
+/**
+* @apiDefine admin Se necesita un JWT de sesion.
+* Para conseguir este token se requiere autenticarse "post /auth" con email y password validos para generar el Json Web Token (JWT)
+* @apiVersion 0.1.0
+*/
